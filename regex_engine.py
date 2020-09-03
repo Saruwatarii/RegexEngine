7117378b90e9b0399cc4e7ba91786348caf0409a
Tim Aadland <potetstapp@gmail.com>
2020-09-03 15:31:17 +0200

    Put the code in a function for later updates.

--- regex_engine.py
+++ regex_engine.py
@@ -1,17 +1,18 @@
+def reggie(string):
+    support = "."
 
-support="."
+    if user_input[0] == "":
+        print(True)
+    elif user_input[1] == "":
+        print(False)
+    elif user_input[0] == "" and user_input[1] == "":
+        print(True)
+    elif user_input[0] == user_input[1]:
+        print(True)
+    elif user_input[0] == support and user_input != "":
+        print(True)
+    else:
+        print(False)
 
 user_input = input().split("|")
-
-if user_input[0] == "":
-    print(True)
-elif user_input[1] == "":
-    print(False)
-elif user_input[0] == "" and user_input[1] == "":
-    print(True)
-elif user_input[0] == user_input[1]:
-    print(True)
-elif user_input[0] == support and user_input != "":
-    print(True)
-else:
-    print(False)
+reggie(user_input)
